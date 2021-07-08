3f64ea8e18827e6f2d3383f96fa230d8ef925653
dean <iamdekanos@gmail.com>
2021-07-08 12:28:49 +0300

    added some css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Dean");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName("nos");
+    setAge(31);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 };
