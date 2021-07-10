92a5d84a8d1aa7588172b525c32eea7b3dd4f057
dean <iamdekanos@gmail.com>
2021-07-10 12:27:26 +0300

    mapping through blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum ...",
+      author: "dean",
+      id: 1,
+    },
+    {
+      title: "My new website",
+      body: "lorem ipsum ...",
+      author: "yarin",
+      id: 2,
+    },
+    {
+      title: "My new website",
+      body: "lorem ipsum ...",
+      author: "niv",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
